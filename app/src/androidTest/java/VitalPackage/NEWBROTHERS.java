69d59ea6c762cbabf4550fb7ce233ac9bdaad1df
VITAL <vitalikezik@gmail.com>
2020-05-21 11:36:43 +0300

    DADADAD:

--- app/src/androidTest/java/VitalPackage/NEWBROTHERS.java
+++ app/src/androidTest/java/VitalPackage/NEWBROTHERS.java
@@ -1,4 +1,11 @@
 package VitalPackage;
 
+import androidx.annotation.NonNull;
+
 public class NEWBROTHERS {
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
