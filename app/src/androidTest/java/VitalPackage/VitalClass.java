760333144f836177e50bdc751e6bdf1c59e520f1
VITAL <vitalikezik@gmail.com>
2020-05-21 11:01:06 +0300

    First part

---
+++ app/src/androidTest/java/VitalPackage/VitalClass.java
@@ -0,0 +1,9 @@
+package VitalPackage;
+
+public class VitalClass {
+    static int n = 1;
+    static long l = 1;
+    static short s = 1;
+    static double d = 1.1d;
+    static float f = 1.1f;
+}
