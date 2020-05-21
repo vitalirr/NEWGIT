03a0e49dddebdced4459aedc7e7dd240d19d2242
VITAL <vitalikezik@gmail.com>
2020-05-21 11:24:54 +0300

    DADADADAD

---
+++ app/src/androidTest/java/VitalPackage/SANDER.java
@@ -0,0 +1,12 @@
+package VitalPackage;
+
+import androidx.annotation.NonNull;
+
+public class SANDER extends VitalClass {
+    int n =0;
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
